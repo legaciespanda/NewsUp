7de054951c6600cfdd95aa2eee64fdf81182c8d7
Ernest Obot <youandines@gmail.com>
2020-11-26 04:24:39 +0100

    App text added

--- src/activity/Splashscreen.js
+++ src/activity/Splashscreen.js
@@ -1,6 +1,6 @@
 // Import React and Component
 import React, {useState, useEffect} from 'react';
-import { ActivityIndicator, View, StyleSheet, Image } from 'react-native';
+import { ActivityIndicator, View, StyleSheet, Image,Text } from 'react-native';
 import { AppStyles } from "../config/AppStyles";
 
 import AsyncStorage from '@react-native-community/async-storage';
@@ -23,8 +23,9 @@ const SplashScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>DigiWigi</Text>
       <Image
-        source={require("../../assets/auth/background_dot.png")}
+        source={require("../../assets/auth/logo.png")}
         style={{ width: "90%", resizeMode: "contain", margin: 30 }}
       />
 
@@ -43,12 +44,18 @@ export default SplashScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: AppStyles.color.deepblue,
   },
   activityIndicator: {
-    alignItems: 'center',
+    alignItems: "center",
     height: 80,
+  },
+  header: {
+    fontSize: 20,
+    color: "#ffff",
+    fontWeight: "bold",
+    paddingVertical: 14,
   },
 });
